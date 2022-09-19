fca7a82495b35c945e1a01a4742a1f5a599cf01d
dkokamoto <dokamoto@bio.fsu.edu>
2022-09-19 09:31:35 -0400

    Initial commit

---
+++ R/example_exploration.R
@@ -0,0 +1,106 @@
+###########################################################################
+###########################################################################
+###                                                                     ###
+### Example analysis of odes using exponential decay and seir models    ###
+### Author: Examples McGillicutty                                       ###
+### Last Update: 9/19/2022                                              ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+# load required packages
+library(RColorBrewer)
+library(deSolve)
+
+# load required functions 
+source("R/odes.R")
+
+### set parameters up here
+scale = 365
+maxtime = 3 * scale
+
+# check out the ode function in deSolve package
+?ode
+
+### expontential decay example
+# First, try the exponential decay function.  
+# Lets set parameters on the annual scale but then scale to daily rates;
+# this way we can visualize dynamics at small temporal scales
+
+pars = c(z = 1 / scale)
+init = c(x = 100) 
+
+# generate ode solution at time = 0
+exp_decay_ode(t = 0,
+              state = init,
+              parameters = pars)
+
+# generate the solutions for the population sizes over time 
+output <- ode(
+  # supply initial values
+  init, 
+  # supply times you're interested in solving for
+  times = seq(0, maxtime,by= 1), 
+  # supply the function
+  func = exp_decay_ode, 
+  # supply the parameters
+  pars)
+
+# plot the numerical solution
+plot(output[, -1], type = "l", ylab= "x", xlab = "time (days)", lwd = 4)
+
+# calculate the analytical solution where N(t) = N(0) * exp(- z * t)
+x_t = with(pars, init * exp(- pars * seq(0, maxtime,by= 1)))
+
+# plot the analytical solution in red
+lines(seq(0, maxtime,by= 1), x_t, lty= 3, col= "red", lwd= 2)
+
+# set parameters
+pars =  c(z = 0.1 / scale,  
+          f = 0 / scale, 
+          beta = 2 / scale, 
+          k = 2 / scale,
+          r = 10 / scale, 
+          delta = 0.1 / scale,
+          gamma = 2 / scale)
+
+# set some initial values (needs to be one for each state)
+init = c(S= 100,
+         E = 0, 
+         I = 1,
+         R = 0)
+
+# generate the rate of change at time = 0
+seir_ode(t= 0, 
+  state = init, 
+  pars
+)
+
+# generate the solutions for the population sizes over time 
+output <- ode(
+  # supply initial values
+  init, 
+  # supply times you're interested in solving for
+  times = seq(0, maxtime,by= 1), 
+  # supply the function
+  func = sei_ode, 
+  # supply the parameters
+  pars)
+
+# plot the output (the first column is the time column, so remove that one)
+matplot(output[,- 1],type= "l", 
+  col= brewer.pal(4,"RdYlBu"), 
+  lty = 1,
+  lwd = 2,
+  ylab = "density",
+  xlab = "time (days)")
+
+# plot the legend so we can see whats happening
+legend(1,
+  max(a[,-1]), 
+  names(init), 
+  col= brewer.pal(4,"RdYlBu"), 
+  xjust= -1, 
+  bg = NA, 
+  bty="n", 
+  lty= 1)
---
+++ R/example_odes.R
@@ -0,0 +1,75 @@
+###########################################################################
+###########################################################################
+###                                                                     ###
+### Sets of ODE models for analysis                                     ###
+### Author: Examples McGillicutty                                       ###
+### Last Update: 9/19/2022                                              ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+### ode functions take three arguments:
+# '@param t vector of time  points
+# '@param state vector of state variables
+# '@param parameters vector of parameter values 
+
+
+###########################################################################
+### simple exponential decay 
+###########################################################################
+
+# This model is simply exponential decay over time.
+# Note: The analytical solution is x(t) = x(0) * exp(- z * t)
+
+### parameters to be defined in `parameters`
+# z = natural mortality
+
+### state variables to be defined in `state`
+# x individuals left alive
+
+exp_decay_ode <-function(t, state, parameters) {
+  with(as.list(c(state, parameters)),{
+    dx <- - z * x 
+    list(c(dx))
+  })
+}
+
+###########################################################################
+### SEIR model with fishing
+###########################################################################
+
+# This model characterizes changes in susceptible, exposed, infected, 
+# and resistant/recovered individuals over time as a function of baseline
+# mortality rate (note that one can convert this into annual surival by 
+# exp(- z * t)) where t = the time in a year relative to the scale of z 
+
+
+### parameters to be defined in `parameters`
+# z = instantaneous natural mortality (per individual, per unit time)
+# f = instantaneous fisheries mortality (per individual, per unit time)
+# beta = instantaneous rate of infection (per infected, per susceptible, per unit time)
+# k = instantaneous rate of transition from exposed to infected
+# r = instantaneous rate of recruitment into susceptible
+# delta = instantaneous death rate of infected over and above natural/fisheries 
+# gamma = instantaneous rate of transition into recovered from infected 
+
+### state variables to be defined in `state`
+# S = susceptible
+# E = exposed
+# I = infected/infectious
+# R = recovered/resistant
+
+seir_ode <-function(t, state, parameters) {
+  with(as.list(c(state, parameters)),{
+    # susceptible
+    dS <- - (z + f + beta * I) * S  + r 
+    # exposed but not yet infectious
+    dE <- - (z + f + k) * E + beta * I * S 
+    # infected
+    dI <- k * E - (z + f + delta + gamma) * I
+    # recovered/resistant
+    dR <- gamma * I - (z + f) * R
+    # return the rate of change for the compartments
+    list(c(dS, dE, dI, dR))
+  })
+}
